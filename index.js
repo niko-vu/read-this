4259e13f2219a4e6fa083dfa6480d8b8f2804abd
fearoffun <480dunce@gmail.com>
2023-06-21 16:19:12 -0700

    filling out sections

--- index.js
+++ index.js
@@ -1,29 +1,86 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project?',
+        // Title questions[1]
+    'Provide a brief description explaining what this project is and your motivation for this project, why you built it, and what you might have solved or learned.',
+        // Description questions[2]
+    'What do you need to do to install this project using a step-by-step description?',
+        // Installation instructions questions[3]
+    'How do you use this application?',
+        // Usage information questions[4]
+    'List your collaborators, tutorials referenced, and any third-party assets used (if applicable).',
+        // Credits questions[5]
+    'What license are you using?',
+        // License questions[6]
+    'Provide guidelines for how other developers might contribute to this application or package (optional).',
+        // Contribution guidelines questions[7]
+    'Write examples on how to run tests (if applicable).',
+        // Test instructions questions[8]
+];
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[1],
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[2],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[3],
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[4],
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: questions[5],
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: questions[6],
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: questions[7],
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: questions[8],
+        },
+    ])
+
+    .then((data) => {
+        const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) => err ? console.log(err) : console.log('Success!')
+        );
+    });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
